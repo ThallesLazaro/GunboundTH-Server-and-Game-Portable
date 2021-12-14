5f7436d06287484a40afdcab2c428c69ff097f5d
ThallesLazaro <13337525+ThallesLazaro@users.noreply.github.com>
2021-12-14 13:20:34 -0300

    Update LigarLinux.sh

--- Servidor/LigarLinux.sh
+++ Servidor/LigarLinux.sh
@@ -3,7 +3,12 @@ bash sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get install python3.6
 sudo apt-get install python3-pip
 python3 -m pip install --upgrade
-python3 -m pip install -r ./Requisitos.txt
+python3 -m pip install altgraph==0.16.1
+python3 -m pip install future==0.17.1
+python3 -m pip install macholib==1.11
+python3 -m pip install pefile==2018.8.8
+python3 -m pip install PyInstaller==3.4
+python3 -m pip install pywin32-ctypes==0.2.0
 python3 -m pip install pycryptodome
 killall -9 python3
 python3 ./Scripts.py
